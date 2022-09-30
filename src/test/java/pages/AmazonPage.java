de84156c7ff6ac3dc6e2a2ca37671b6038421326
Beyza Kılıç <beyza7259@gmail.com>
2022-09-30 10:15:15 +0300

    seleniumTestNG

---
+++ src/test/java/pages/AmazonPage.java
@@ -0,0 +1,31 @@
+package pages;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+public class AmazonPage {
+
+    public AmazonPage(){ //Constructor oluşturup public yapmamız gerekir
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    /*
+    PageFactory bizlere nesne deposu oluşturmaya imkan veren, sürdürülebilir,
+    yeniden kullanılabilir ve elementleri tek bir yerden yönetmeyi sağlayan bir Page object model konseptidir.
+
+    Page Factory’i kullanımına bakacak olursak PageFactory model, Web elementlerini @FindBy annotionu ile algılar.
+    How kalıbı ile birlikte elementlerin ayırt edici locator’ından hangisini kullanacaksak belirtilir.
+
+    Sonrasında ise o ayırt edici locator’ın adı tanımlanır.
+    Public WebElement ile birlikte test senaryolarında kullanmak üzere isimlendirmesi yaparız.
+    Burada isimlendirme daha sonra baktığında anlayabilmek için önemlidir, okunaklı olmalıdır.
+     */
+
+    @FindBy (id="twotabsearchtextbox")
+    public WebElement aramaKutusu;
+
+    @FindBy (xpath = "//div[@class='a-section a-spacing-small a-spacing-top-small']")
+    public WebElement aramaSonucWE;
+
+}
---
+++ src/test/java/tests/day17/C04_DriverIlkTest.java
@@ -0,0 +1,26 @@
+package tests.day17;
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.TestBaseBeforeClassAfterClass;
+public class C04_DriverIlkTest {
+
+    @Test
+    public void test01() {
+     /*
+     TestBase class'ına extend ederek kullandığımız driver yerine artık
+     Driver class'ından kullanacağımız
+     getDriver() static method'unu kullanırız
+     */
+        Driver.getDriver().get("https://amazon.com");
+        Driver.getDriver().get("https://bestbuy.com");
+        Driver.closeDriver();
+        Driver.getDriver().get("https://hepsiburada.com");
+        Driver.closeDriver();
+    }
+}
+    // testlerimizi calistirdigimizda her seferinde yeni driver oludturdugu icin
+    // her test methodu icin yeni bir pencere (driver) aciyor
+    // eger driver'a bir deger atanmamissa yani driver == null ise
+    // bir kere driver'i calistir diyerek bir kere if icini calistiracak
+    // ve driver artik bir kere calistigi icin ve deger atandigi icin null olmayacak
+    // ve direk return edecek ve diger testlerimiz ayni pencere (driver) uzerinde calisacak
